ff4cf1a549ac8bd9a84ccaef12b7b62e61fe52d1
lixutang <cuclixutang@163.com>
2016-06-06 23:36:41 +0800

    获取词条链接

--- 3-beautifulSoup.py
+++ 3-beautifulSoup.py
@@ -1,8 +1,9 @@
 from  urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
 html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
 bsObj = BeautifulSoup(html,"html.parser") 
-for link in bsObj.findAll("a"):
+for link in bsObj.find("div",{"id"""bodyCotent"}).findAll()("a",href =re.compile("^(/wiki)(?!:).)*$")):
     if 'href' in a.attrs:
         print(link.attrs['href'])
